6a2e32a13279273d0453b2604e3e6117e213eaca
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-24 16:05:01 +0100

    add gaming section to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,14 @@ const Home = () => {
   const infoCards = [
     { title: 'Welcome to Lemon Industries', description: 'This website is currently under construction, as I learn more about React and MaterialUI, I will extend it more and more. But even at this early state you can already look at some other projects I have done or try out my small little API^^ Have fun!', type: 'info' },
     { title: 'Projects', description: "On my GitHub page you can find some projects I have started for learning different kinds of concepts. These range from 'genetic simulation', to a simple fighting game skelleton, to a colonization simulator with UI, and much more! Most is unfinished as I enjoy trying out more and more stuff. As long as Im happy with the current state I'll be able to move on to the next project :)", type: 'info' },
+    { title: 'Gaming', description: "After coding, gaming is probably one of my biggest hobby. Over the years I have played a TON of insanely well-made games. Some games which have had a lasting impression on me or just gave me some new experiences. I will present to you my selection of games which I consider masterpieces, and if you are a fan of the respective genre, a must-play!", type: 'info' },
     { title: 'API', description: "My current major project is my own API! As of right now it only acts as a way to fetch random images by category, from the ones I host. Some discord bots have commands like /hug @person and it will send a random hug gif. My API can act as a provider for such random images, while also making it easy to add them to discord embeds as they are statically hosted on my server.", type: 'info' }
   ];
 
   const imageCards = [
     { imageUrl: '/lemons.jpg', height: 200, buttonLabel: 'Page Code on GitHub', buttonUrl: 'https://github.com/Zitronenjoghurt/lemon-industries-website', type: 'image' },
     { imageUrl: '/coding.jpg', height: 225, buttonLabel: 'Project Overview', buttonUrl: '/projects', type: 'image' },
+    { imageUrl: '/astroneer_5.jpg', height: 225, buttonLabel: 'Favorite Games', buttonUrl: '/gaming', type: 'image' },
     { imageUrl: '/network.jpg', height: 225, buttonLabel: 'API Overview', buttonUrl: '/api', type: 'image' }
   ];
 
